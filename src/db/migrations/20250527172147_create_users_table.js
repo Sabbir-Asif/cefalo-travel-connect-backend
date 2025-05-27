47082a3fd52e2e28b32cc490ac2e107b77c0deb8
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-05-28 01:06:41 +0600

    feat: add create user table migration

---
+++ src/db/migrations/20250527172147_create_users_table.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    await knex.raw(`CREATE TYPE "Role" AS ENUM ('ADMIN', 'TRAVELER', 'EXPLORER')`);
+    
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').unique().notNullable();
+        table.string('password').notNullable();
+        table.enu('role', ['ADMIN', 'TRAVELER', 'EXPLORER'], {
+            useNative: true,
+            enumName: 'Role',
+        }).notNullable().defaultTo('EXPLORER');
+        table.string('displayPicture');
+        table.string('bio');
+        table.timestamp('createdAt').defaultTo(knex.fn.now());
+        table.timestamp('updatedAt').defaultTo(knex.fn.now());
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.dropTable('users');
+    await knex.raw('DROP TYPE "Role"');
+};
