5408b3700a918d0964ddc66befbd22c42f33693b
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-05-28 18:53:15 +0600

    feat: add global error handler, protect authoute by global error handler

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { login, signup } from "../controllers/auth";
+import { errorHandler } from "../global-error-handler";
 
 export const authRouter : Router = Router();
 
-authRouter.post('/signup', signup);
-authRouter.post('/login', login);
+authRouter.post('/signup', errorHandler(signup));
+authRouter.post('/login', errorHandler(login));
