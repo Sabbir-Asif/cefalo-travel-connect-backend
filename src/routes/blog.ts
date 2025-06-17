50685b01d6b4c7c27a579d2cd23c9ba0311fb670
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-06-17 22:34:48 +0600

    feat: add transport to blog routes

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -2,9 +2,13 @@ import { Router } from "express";
 import { createBlog, deleteBlog, getAllBlogs, getBlogById, searchBlogs, updateBlog } from "../controllers/blog";
 import { errorHandler } from "../global-error-handler";
 import { authMiddleware } from "../middlewares/auth";
+import { createBlogtransport, deleteBlogTransport, getTransportsForBlog } from "../controllers/blogTransport";
 
 export const blogRouter: Router = Router();
 
+blogRouter.post('/transports', authMiddleware, errorHandler(createBlogtransport));
+blogRouter.get('/:id/transports', authMiddleware, errorHandler(getTransportsForBlog));
+blogRouter.delete('/:blogId/transports/:transportId', authMiddleware, errorHandler(deleteBlogTransport));
 blogRouter.get('/search', authMiddleware, errorHandler(searchBlogs));
 blogRouter.post('/', authMiddleware, errorHandler(createBlog));
 blogRouter.get('/',authMiddleware, errorHandler(getAllBlogs));
