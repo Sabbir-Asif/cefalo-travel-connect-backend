13153cdffe09131d6ab5f31ca2cb889d5f6fb764
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-06-19 16:07:00 +0600

    feat: add food routes

--- src/controllers/food.ts
+++ src/controllers/food.ts
@@ -1,21 +1,86 @@
 import { Request, Response } from "express";
+import { FoodRepository } from "../repositories/impl/food-impl";
+import { FoodService } from "../services/food";
+import { CreateFoodSchema, UpdateFoodSchema } from "../schemas/food";
+import { UnprocessableEntityException } from "../exceptions/validation";
+import { ErrorCode } from "../exceptions/root";
+import { IdSchema } from "../schemas/id";
+import { BadRequestException } from "../exceptions/bad-request";
+import { CreateFoodDto, UpdateFoodDto } from "../dtos/food";
+import { CreateFood, Food, UpdateFood } from "../interfaces/food";
+import { UUID } from "crypto";
+
+const foodRepository = new FoodRepository();
+const foodService = new FoodService(foodRepository);
 
 export const createFood = async (req: Request, res: Response) => {
-    res.json(`create food called`);
-}
+  const parsed = CreateFoodSchema.safeParse(req.body);
+  if (!parsed.success) {
+    throw new UnprocessableEntityException(parsed.error, "Validation error!", ErrorCode.UNPROCESSABLE_ENTITY);
+  }
+
+  const foodCreateDto: CreateFood = new CreateFoodDto(parsed.data);
+  const food: Food = await foodService.createFood(foodCreateDto);
+
+  res.status(201).json(food);
+};
 
 export const getAllFoods = async (req: Request, res: Response) => {
-    res.json(`get all food called`);
-}
+  const foods: Food[] = await foodService.getAllFoods();
+  res.status(200).json(foods);
+};
 
 export const getFoodById = async (req: Request, res: Response) => {
-    res.json(`get food by id called`);
-}
+  const id = req.params.id;
+  const parsedId = IdSchema.safeParse(id);
+  if (!parsedId.success) {
+    throw new BadRequestException("Invalid food id", ErrorCode.INVALID_FOOD_ID);
+  }
+
+  const foodId = parsedId.data as UUID;
+
+  const food = await foodService.getFoodById(foodId);
+
+  res.status(200).json(food);
+};
 
 export const updateFood = async (req: Request, res: Response) => {
-    res.json(`update food called`);
-}
+  
+const id = req.params.id;
+  const parsedId = IdSchema.safeParse(id);
+  if (!parsedId.success) {
+    throw new BadRequestException("Invalid food id", ErrorCode.INVALID_FOOD_ID);
+  }
+
+  const foodId = parsedId.data as UUID;
+
+  const parsed = UpdateFoodSchema.safeParse(req.body);
+  if (!parsed.success) {
+    throw new UnprocessableEntityException(parsed.error, "Validation error!", ErrorCode.UNPROCESSABLE_ENTITY);
+  }
+
+  const updateFoodDto: UpdateFood = new UpdateFoodDto(parsed.data);
+  const updatedFood = await foodService.updateFood(foodId, updateFoodDto);
+
+  res.status(200).json(updatedFood);
+};
 
 export const deleteFood = async (req: Request, res: Response) => {
-    res.json(`delete food called`);
-}
+  const id = req.params.id;
+  const parsedId = IdSchema.safeParse(id);
+  if (!parsedId.success) {
+    throw new BadRequestException("Invalid food id", ErrorCode.INVALID_FOOD_ID);
+  }
+
+  const foodId = parsedId.data as UUID;
+
+  await foodService.deleteFood(foodId);
+  
+  res.status(204).json({ success: true });
+};
+
+export const searchFoods = async (req: Request, res: Response) => {
+  const queryParams = req.query;
+  const foods = await foodService.searchFoods(queryParams);
+  res.status(200).json(foods);
+};
