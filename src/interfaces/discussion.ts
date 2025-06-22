b8af40cdb360752a0de393f170157773f8b1f16c
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-06-22 11:35:59 +0600

    feat: add token sevice

---
+++ src/interfaces/discussion.ts
@@ -0,0 +1,23 @@
+import { UUID } from "crypto";
+
+export interface Discussion {
+  id: number;
+  travel_plan_id: UUID;
+  sender_id: UUID;
+  content: string;
+  created_at: Date;
+}
+
+export interface CreateDiscussion {
+  travel_plan_id: UUID;
+  content: string;
+}
+
+export interface DiscussionWithSender extends Discussion {
+  sender: {
+    id: UUID;
+    name: string;
+    email: string;
+    displayPicture: string | null;
+  };
+}
