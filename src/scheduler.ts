cd6133d25c14334fdc0e9f610dd0d962cbe409f8
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-06-23 14:30:09 +0600

    feat: add job to delete expired tokens

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,6 +1,7 @@
 import cron from "node-cron";
-import { cleanExpiredVerifications } from "./jobs/clean-expired-verifications";
+import { cleanExpiredPasswordResets, cleanExpiredVerifications } from "./jobs/clean-expired-verifications";
 
 cron.schedule("0 * * * *", async () => {
   await cleanExpiredVerifications();
+  await cleanExpiredPasswordResets();
 });
