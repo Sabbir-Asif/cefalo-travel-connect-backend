d60b227688d29a687d4fa01c3ac5d1de678b33ff
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-06-27 22:07:05 +0600

    refactor: remove test file

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-};
-
-export default config;
