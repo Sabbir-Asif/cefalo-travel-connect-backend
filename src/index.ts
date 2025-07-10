e98c00adcaab6e18f841f600424b8da26f917448
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-07-10 12:12:22 +0600

    feat: add cors config

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
+import cors from "cors";
 import { PORT } from './configs/secrets';
 import { rootRouter } from './routes/root';
-import { db } from './configs/db';
 import { errorMiddleware } from './middlewares/error';
 import cookieParser from 'cookie-parser';
 import { Database } from './utils/database';
+import { corsOptions } from './configs/cors';
 
 const app: Express = express();
 const database = new Database();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api',rootRouter);
+app.use(cors(corsOptions));
+
+
+app.use('/api', rootRouter);
 app.use(errorMiddleware);
 
-app.listen(PORT, async ()=> {
+app.listen(PORT, async () => {
     console.log(`server is running on port ${PORT}`);
-     await database.connect();
+    await database.connect();
 })
