7a6160c7c2c805fd3ac879bfe8dffc65692a18a6
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-07-10 10:04:01 +0600

    chore: remove public key file and key config

--- src/configs/keys.ts
+++ src/configs/keys.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
 import path from 'path';
 
-export const privateKey = fs.readFileSync(path.join(__dirname, '../../keys/private.key'), 'utf8');
-export const publicKey = fs.readFileSync(path.join(__dirname, '../../keys/public.key'), 'utf8');
+export const privateKey = fs.readFileSync(path.join(__dirname, '../../keys/private.pem'), 'utf8');
+export const publicKey = fs.readFileSync(path.join(__dirname, '../../keys/public.pem'), 'utf8');
